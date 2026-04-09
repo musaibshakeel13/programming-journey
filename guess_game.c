7808a2aefbe741af5f9c6a299c3293f4ff31f904
Musaib shakeel <hajimusaib0@gmail.com>
2026-04-09 20:57:40 +0530

    Create guess_game.c

---
+++ guess_game.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int number, guess, attempts = 0;
+
+    srand(time(0));
+    number = rand() % 100 + 1;
+
+    printf("=== Number Guessing Game ===\n");
+    printf("Guess number between 1 to 100\n");
+
+    do {
+        printf("Enter guess: ");
+        scanf("%d", &guess);
+        attempts++;
+
+        if(guess > number)
+            printf("Too high! Try again\n");
+        else if(guess < number)
+            printf("Too low! Try again\n");
+        else
+            printf("Correct! You guessed in %d attempts\n", attempts);
+
+    } while(guess != number);
+
+    return 0;
+}
